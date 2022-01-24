b7f0db622d80b93d00f7dff4aae7d9a04812a2ac
Michael Tan <michaeltan@MacBook-Pro.fios-router.home>
2022-01-24 18:54:55 -0500

    added routes to the frontend

--- frontend/12pell_internship/src/App.js
+++ frontend/12pell_internship/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom"
+import Home from './screens/Home/Home.js'
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/internships' element={null} />
+        <Route path='/mentors' element={null} />
+        <Route path='/contact' element={<contact />} />
+        <Route path='/about' element={<about />} />
+      </Routes>
     </div>
   );
 }
