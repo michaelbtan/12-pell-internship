bbc0d233e68742ed952bc14542cda2641c16a3e4
Michael Tan <michaeltan@MacBook-Pro.fios-router.home>
2022-01-24 14:33:01 -0500

    added serializers

--- backend/pella/serializers.py
+++ backend/pella/serializers.py
@@ -1,2 +1,22 @@
 from rest_framework import serializers
-from pella.models import Mentor, Internship, Referral
+from pella.models import Mentor, Internship, Referral, Mentee
+
+class MentorSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = Mentor
+    fields = ['name', 'email', 'bio', 'img']
+
+class Internship(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = Internship
+    fields = ['title', 'description', 'industry', 'img', 'paid_unpaid', 'mentor']
+
+class ReferralSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = Referral
+    fields = ['title', 'comment', 'rating', 'mentor']
+
+class Mentee(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = Mentee
+    fields = ['name', 'email', 'interest']
