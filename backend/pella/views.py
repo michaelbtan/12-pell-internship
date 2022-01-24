dd74b0958fdad2d7a7b978e7a49a082d9d15508f
Michael Tan <michaeltan@MacBook-Pro.fios-router.home>
2022-01-24 15:09:54 -0500

    reinstalled dependencies to pipfile

--- backend/pella/views.py
+++ backend/pella/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from pella.serializers import MentorSerializer, InternshipSerializer, ReferralSerializer, MenteeSerializer
 from pella.models import Mentor, Internship, Referral, Mentee
+
 
 class MentorViewSet(viewsets.ModelViewSet):
   queryset = Mentor.objects.all()
   serializer_class = MentorSerializer
+  permission_classes = [permissions.IsAuthenticated]
 
 class InternshipViewSet(viewsets.ModelViewSet):
   queryset = Internship.objects.all()
   serializer_class = InternshipSerializer
+  permission_classes = [permissions.IsAuthenticated]
 
 class ReferralViewSet(viewsets.ModelViewSet):
   queryset = Referral.objects.all()
   serializer_class = ReferralSerializer
+  permission_classes = [permissions.IsAuthenticated]
 
 class MenteeViewSet(viewsets.ModelViewSet):
   queryset = Mentee.objects.all()
   serializer_class = MenteeSerializer
+  permission_classes = [permissions.IsAuthenticated]
